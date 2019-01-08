5d6532d648d89537f1a032274b7a9342edfd0fc5
Hiroshi Ueki <hirocueki@gmail.com>
2019-01-08 11:23:11 +0900

    ループですべての画像を処理するように変更

--- detect_images.rb
+++ detect_images.rb
@@ -4,25 +4,30 @@ if images.empty?
   puts "対象ファイルがみつかりません"
   exit
 end
-images = images.select{|file| !File.exist?("#{file}.txt")}.take(10)
-
-if images.empty? then
-  p '処理対象なし'
-  exit
-end
-
-texts = images.map{|path| "#{path}.txt"}
 
 require "google/cloud/vision"
 
-image_annotator_client = Google::Cloud::Vision::ImageAnnotator.new(version: :v1)
-response = image_annotator_client.document_text_detection(images: images)
+loop do
+  targets = images.select{|file| !File.exist?("#{file}.txt")}.take(5)
 
-response.responses.each_with_index do |result, index|
-  File.open(texts[index], 'w') do |file|
-    puts "processing ... #{file.path}"
-    puts result.text_annotations.first&.description.slice(1,5)
-    file.puts result.text_annotations.first&.description
-    puts "done!"
+  if targets.empty? then
+    p '完了'
+    exit
   end
+
+  texts = targets.map{|path| "#{path}.txt"}
+
+  image_annotator_client = Google::Cloud::Vision::ImageAnnotator.new(version: :v1)
+  response = image_annotator_client.document_text_detection(images: targets)
+
+  response.responses.each_with_index do |result, index|
+    File.open(texts[index], 'w') do |file|
+      puts "processing ... #{file.path}"
+      puts result.text_annotations.first&.description.slice(1,5)
+      file.puts result.text_annotations.first&.description
+      puts "done!"
+    end
+  end
+
+  sleep 1
 end
